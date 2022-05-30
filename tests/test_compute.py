fcbd53caf5b03c134f2e472b37741efd96e53ff6
ZNBai <90194320+ZNBai@users.noreply.github.com>
2022-05-30 23:12:58 +0200

    Update test_compute.py

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -1,18 +1,16 @@
 from os import path
 import sys
-from titanic import Titanic
+from Titanic import titanic
 
 sample_train = "tests/data/train.csv"
 sample_test = "tests/data/test.csv"
 output_path = "tests/data/"
 
 def train_titanic():
-    test1 = Titanic(sample_train)
-    test1.train(sample_train)
+    titanic.train(sample_train)
     assert path.exists("tests/data/clf.pickle") is True
 
 def test_titanic():
-    test2 = Titanic(sample_test, output_path)
-    test2.test(sample_test, output_path)
+    titanic.test(sample_test, output_path)
     assert path.exists("tests/data/prediction.csv") is True
     
