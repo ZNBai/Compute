c6e2e3da1c3a1653014b33c513c1f53d1fd7530c
ZNBai <bzn991206@gmail.com>
2022-05-31 00:48:18 +0200

    x

--- test_compute.py
+++ test_compute.py
@@ -2,16 +2,16 @@ from os import path
 import sys
 import Titanic
 
-sample_train = "tests/data/train.csv"
-sample_test = "tests/data/test.csv"
-model_path = "tests/data/"
-output_path = "tests/data/"
+sample_train = "/data/train.csv"
+sample_test = "/data/test.csv"
+model_path = "/data/"
+output_path = "/data/"
 
-def train_titanic():
+def test_train():
     Titanic.train(sample_train, model_path)
     assert path.exists(model_path + "clf.pickle") is True
 
-def test_titanic():
+def test_test():
     Titanic.test(sample_test, model_path, output_path)
     assert path.exists(output_path + "prediction.csv") is True
     
