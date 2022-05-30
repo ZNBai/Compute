f403a513b91ca2c7985ed56f955866dd34bca9cc
ZNBai <90194320+ZNBai@users.noreply.github.com>
2022-05-31 00:41:24 +0200

    Update test_compute.py

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -7,11 +7,11 @@ sample_test = "tests/data/test.csv"
 model_path = "tests/data/"
 output_path = "tests/data/"
 
-def train_titanic():
+def test_train():
     Titanic.train(sample_train, model_path)
     assert path.exists(model_path + "clf.pickle") is True
 
-def test_titanic():
+def test_test():
     Titanic.test(sample_test, model_path, output_path)
     assert path.exists(output_path + "prediction.csv") is True
     
