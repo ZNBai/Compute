d4b46ea982c1d2b58d1461c89682e0dd42eba4cd
ZNBai <bzn991206@gmail.com>
2022-05-31 17:38:05 +0200

    test

--- test_compute.py
+++ test_compute.py
@@ -24,10 +24,7 @@ def test_train():
     assert path.exists(output_path + "clf.pickle") is True
 
 def test_test():
-    Titanic.clean(sample_test, output_path)
-    Titanic.numeralization(output_path + "clean.csv", output_path)
-    Titanic.normalization(output_path + "numeralization.csv", output_path)
-    Titanic.test(output_path + "normalization.csv", model_path, output_path)
+    Titanic.test(sample_test, model_path, output_path)
     assert path.exists(output_path + "prediction.csv") is True
 
     
