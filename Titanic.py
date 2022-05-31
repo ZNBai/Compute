59b4a3af208d3075c7653bbce7f81687c8c3ad37
ZNBai <bzn991206@gmail.com>
2022-05-31 17:02:57 +0200

    split

--- Titanic.py
+++ Titanic.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 from sklearn.ensemble import RandomForestRegressor
 import sklearn.preprocessing as preprocessing
+from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 import pickle
 import os
@@ -14,12 +15,15 @@ def read_input(input_path):
     data = pd.read_csv(input_path)
     return data
 
-def data_clean(df):
+def clean(input_path, output_path):
+    df = read_input(input_path)
     df = df.drop(df[df.Embarked.isnull()].index)
     df.fillna(df.median(), inplace = True)
-    return df
+    df.to_csv(output_path + "preprocess.csv", index=False)
+    return "Wrote the preprocessed data to: " + output_path + "preprocess.csv.\n" 
 
-def set_numeralization(data):
+def numeralization(input_path, output_path):
+    data = read_input(input_path)
     # Factorization for class-specific attributes, Embarked,Sex,Pclass
     dummies_Embarked = pd.get_dummies(data['Embarked'], prefix='Embarked')
     dummies_Sex = pd.get_dummies(data['Sex'], prefix='Sex')
@@ -29,23 +33,26 @@ def set_numeralization(data):
     df = pd.concat([data, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
     # Remove the old attributes
     df.drop(['Pclass', 'Sex', 'Embarked'], axis=1, inplace=True)
-    return df
+    df.to_csv(output_path + "numeralization.csv", index=False)
+    return "Wrote the numeralized data to: " + output_path + "numeralization.csv.\n" 
 
 # Feature normalisation
-def set_normalization(df):
+def normalization(input_path, output_path):
+    df = read_input(input_path)
     scaler = preprocessing.StandardScaler()
     age_scale_param = scaler.fit(df['Age'].values.reshape(-1,1))
     df['Age_scaled'] = scaler.fit_transform(df['Age'].values.reshape(-1,1),age_scale_param)
     fare_scale_param = scaler.fit(df['Fare'].values.reshape(-1,1))
     df['Fare_scaled'] = scaler.fit_transform(df['Fare'].values.reshape(-1,1),fare_scale_param)
-    return df
+    df.to_csv(output_path + "normalization.csv", index=False)
+    return "Wrote the normalized data to: " + output_path + "normalization.csv.\n" 
 
-def train(input_path, model_path):
+def train(input_path, output_path):
 
     df = read_input(input_path)
-    df = data_clean(df)
-    df = set_numeralization(df)
-    df = set_normalization(df)
+    #df = data_clean(df)
+    #df = set_numeralization(df)
+    #df = set_normalization(df)
 
     train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Embarked_.*|Sex_.*|Pclass_.*')
     train_np = train_df.values
@@ -54,15 +61,16 @@ def train(input_path, model_path):
     x = train_np[:,1:]
     # Get the dependent variable y
     y = train_np[:,0]
+    train_X, test_X, train_y, test_y = train_test_split(x, y, train_size=.8)
     # Fitting using logistic regression to obtain the trained model
     clf = linear_model.LogisticRegression(solver='liblinear',C=1.0,penalty='l2',tol=1e-6)
-    clf.fit(x,y)
+    clf.fit(train_X,train_y)
 
-    f = open(model_path + 'clf.pickle','wb')
+    f = open(output_path + 'clf.pickle','wb')
     pickle.dump(clf,f)
     f.close()
 
-    return "Wrote the model to: " + model_path + "clf.pickle.\n" 
+    return "Accuracy score: " + str(clf.score(test_X,test_y)) + ".\nWrote the model to: " + output_path + "clf.pickle.\n" 
 
 #def test(input: str, output_path: str) -> str:
 def test(input_path, model_path, output_path):
@@ -72,9 +80,9 @@ def test(input_path, model_path, output_path):
         f.close()
 
         df = read_input(input_path)
-        df = data_clean(df)
-        df = set_numeralization(df)
-        df = set_normalization(df)
+        #df = data_clean(df)
+        #df = set_numeralization(df)
+        #df = set_normalization(df)
 
         test_df = df.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Embarked_.*|Sex_.*|Pclass_.*')
         predictions = clf.predict(test_df)
@@ -90,21 +98,27 @@ def test(input_path, model_path, output_path):
 if __name__ == "__main__":
 
     # Make sure that at least one argument is given, that is either 'train' or 'test'
-    if len(sys.argv) != 2 or (sys.argv[1] != "train" and sys.argv[1] != "test"):
-        print(f"Usage: {sys.argv[0]} train|test")
+    if len(sys.argv) != 2 or (sys.argv[1] != "train" and sys.argv[1] != "test" and sys.argv[1] != "clean" and sys.argv[1] != "numeralization" and sys.argv[1] != "normalization"):
+        print(f"Usage: {sys.argv[0]} clean|numeralization|normalization|train|test")
         exit(1)
 
     input_path = os.environ["INPUT"]
-    model_path = os.environ["MODEL_PATH"]
+    output_path = os.environ["OUTPUT_PATH"]
 
     # If it checks out, call the appropriate function
     command = sys.argv[1]
-    if command == "train":
-        result = train(input_path, model_path)
-        #result = train(os.environ["INPUT"])
+    if command == "clean":
+        result = clean(input_path, output_path)
+    elif command == "numeralization":
+        result = numeralization(input_path, output_path)
+    elif command == "normalization":
+        result = normalization(input_path, output_path)
+    elif command == "train":
+        result = train(input_path, output_path)
     else:
-        output_path = os.environ["OUTPUT_PATH"]
+        model_path = os.environ["MODEL_PATH"]
         result = test(input_path, model_path, output_path)
+
         #result = test(os.environ["INPUT"], os.environ["OUTPUT_PATH"])
 
     # Print the result with the YAML package
--- test_compute.py
+++ test_compute.py
@@ -6,6 +6,17 @@ sample_train = "./t_data/train.csv"
 sample_test = "./t_data/test.csv"
 model_path = "./t_data/"
 output_path = "./t_data/"
+def test_clean():
+	Titanic.clean(sample_train, output_path)
+    assert path.exists(model_path + "clean_data.csv") is True
+
+def test_numeralization():
+	Titanic.numeralization(sample_train, output_path)
+    assert path.exists(model_path + "numeralization.csv") is True
+
+def test_normalization():
+	Titanic.normalization(sample_train, output_path)
+    assert path.exists(model_path + "normalization.csv") is True
 
 def test_train():
     Titanic.train(sample_train, model_path)
